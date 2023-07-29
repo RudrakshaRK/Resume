b5df841410329164bb993724ff446f51cfe2628f
RudrakhsaRk <rudrakshakesharihms@gmail.com>
2023-07-29 09:40:53 +0530

    navbar fixes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 document.querySelector(".icon-menu").addEventListener("click", function(){
     document.querySelector(".navbar-container").classList.add("active")
+    document.querySelector(".main-page").classList.toggle("main-page-active")
+    setTimeout(function(){
+        document.querySelector(".navbar-container").classList.add("active-nav")
+    },100)
 })
 document.querySelector(".navbar-close").addEventListener("click", function(){
-    document.querySelector(".navbar-container").classList.remove("active")
+    document.querySelector(".navbar-container").classList.remove("active-nav")
+    document.querySelector(".main-page").classList.toggle("main-page-active")
+    setTimeout(function(){
+        document.querySelector(".navbar-container").classList.remove("active")
+    },500)
 })
