83a2056956405d4d50253768eb9e423a7097a546
RudrakhsaRk <rudrakshakesharihms@gmail.com>
2023-08-01 09:24:25 +0530

    navbar icon animation and loading screen

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 document.querySelector(".icon-menu").addEventListener("click", function(){
-    document.querySelector(".navbar-container").classList.add("active")
-    document.querySelector(".main-page").classList.toggle("main-page-active")
-    setTimeout(function(){
+    
+    const iconMenu = document.getElementById("icon").classList
+
+    if (iconMenu[1] == "close"){
+       
+            document.querySelector(".icon-menu").classList.remove("close")
+            document.querySelector(".navbar-container").classList.remove("active-nav")
+            document.querySelector(".main-page").classList.toggle("main-page-active")
+            setTimeout(function(){
+                document.querySelector(".navbar-container").classList.remove("active")
+            },500)
+    } else {
+        document.querySelector(".icon-menu").classList.add("close")
+        document.querySelector(".navbar-container").classList.add("active")
+        document.querySelector(".main-page").classList.add("main-page-active")
+        setTimeout(function(){
         document.querySelector(".navbar-container").classList.add("active-nav")
-    },100)
-})
-document.querySelector(".navbar-close").addEventListener("click", function(){
-    document.querySelector(".navbar-container").classList.remove("active-nav")
-    document.querySelector(".main-page").classList.toggle("main-page-active")
-    setTimeout(function(){
-        document.querySelector(".navbar-container").classList.remove("active")
-    },500)
+        },100)
+    }
+    
 })
 
+
+
 $("li").click(function(){
-    document.querySelector(".navbar-container").classList.remove("active-nav")
+    document.querySelector(".close").classList.remove("active-nav")
     document.querySelector(".main-page").classList.toggle("main-page-active")
     setTimeout(function(){
-        document.querySelector(".navbar-container").classList.remove("active")
+        document.querySelector(".close").classList.remove("active")
     },500)
 });
+
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".ha");
+
+hamburger_menu.addEventListener("click", () => {
+  hamburger_menu.classList.toggle("active-icon-animation");
+
+
+})
+
+setTimeout(function(){
+    document.querySelector(".scroll").classList.remove("scroll")
+    document.querySelector(".loader-wraper").classList.add("loader-wraper-off-loader")
+    document.querySelector(".loader-wraper h1").classList.add("loader-wraper-off")
+    document.querySelector(".loader-wraper .loader").classList.add("loader-wraper-off")
+  },6000)
